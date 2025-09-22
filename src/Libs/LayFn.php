e5a87e0089927c9e81e15acb202605a9e0b94c04
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-09-22 14:05:20 +0100

    currency formatter

--- src/Libs/LayFn.php
+++ src/Libs/LayFn.php
@@ -66,6 +66,18 @@ final class LayFn
         return $item ? preg_replace ($regexp, '', $num_format) . ($item['symbol'] ?? '') : '0';
     }
 
+    public static function currency(float $num, ?string $locale = null, ?string $currency = null, bool $symbol = true): string
+    {
+        if (!$symbol)
+            return number_format($num, 2);
+
+        $locale ??= self::env('DEFAULT_LOCALE', 'en-NG');
+
+        $fmt = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+
+        return $fmt->formatCurrency($num, $currency ?? self::env('DEFAULT_CURRENCY', 'NGN'));
+    }
+
     public static function trim_word(string $string, string $word, ?string $preg_pattern = null) : string
     {
         $len = function ($str): int {
