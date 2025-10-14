b205738f6cfe17cb52342b5e354e16292203d6cb
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-10-14 11:17:47 +0100

    Added a simple way to get files

--- src/Libs/Primitives/Abstracts/RequestHelper.php
+++ src/Libs/Primitives/Abstracts/RequestHelper.php
@@ -6,6 +6,7 @@ namespace BrickLayer\Lay\Libs\Primitives\Abstracts;
 use BrickLayer\Lay\Core\App;
 use BrickLayer\Lay\Core\Exception;
 use BrickLayer\Lay\Core\LayException;
+use BrickLayer\Lay\Libs\LayArray;
 use BrickLayer\Lay\Libs\Primitives\Traits\ValidateCleanMap;
 
 abstract class RequestHelper
@@ -23,14 +24,20 @@ abstract class RequestHelper
      * Cached request form data of an already parsed $_POST data
      * @var array<string, mixed>|object
      */
-    private static array|object $cached_request_fd;
+    private static array|object $cached_request_form_data;
+
+    /**
+     * Cached request files of an already parsed $_FILE data
+     * @var array<string, mixed>|object
+     */
+    private static array|object $cached_request_files;
 
     abstract protected function rules(): void;
 
     /**
      * @return array<string, mixed>
      */
-    public final function props() : array
+    public final function props(): array
     {
         return $this->data;
     }
@@ -42,19 +49,19 @@ abstract class RequestHelper
      * @param mixed $value
      * @return void
      */
-    public final function new_key(string $key, mixed $value) : void
+    public final function new_key(string $key, mixed $value): void
     {
         $append = str_contains($key, "[]");
 
-        if($append)
+        if ($append)
             $key = str_replace("[]", "", $key);
 
-        if(isset($this->data[$key]))
+        if (isset($this->data[$key]))
             LayException::throw_exception(
                 "Trying to update an existing property to your Request. You can only do that in the `update` function"
             );
 
-        if($append)
+        if ($append)
             $this->data[$key][] = $value;
         else
             $this->data[$key] = $value;
@@ -67,19 +74,19 @@ abstract class RequestHelper
      * @param mixed $value
      * @return void
      */
-    public final function update(string $key, mixed $value) : void
+    public final function update(string $key, mixed $value): void
     {
         $append = str_contains($key, "[]");
 
         if ($append)
             $key = str_replace("[]", "", $key);
 
-        if(!isset($this->data[$key]))
+        if (!isset($this->data[$key]))
             LayException::throw_exception(
                 "Trying to dynamically add a new property to your Resource. You can only do that in the `post_validate` or `new_key` function"
             );
 
-        if($append)
+        if ($append)
             $this->data[$key][] = $value;
         else
             $this->data[$key] = $value;
@@ -90,17 +97,17 @@ abstract class RequestHelper
      * @param string ...$keys
      * @return $this
      */
-    public final function except(string ...$keys) : static
+    public final function except(string ...$keys): static
     {
         foreach ($keys as $key) {
-            if(isset($this->data[$key]))
+            if (isset($this->data[$key]))
                 unset($this->data[$key]);
         }
 
         return $this;
     }
 
-    public final function unset(string ...$keys) : static
+    public final function unset(string ...$keys): static
     {
         return $this->except(...$keys);
     }
@@ -110,7 +117,7 @@ abstract class RequestHelper
      * @param string $key
      * @return mixed
      */
-    protected final function get(string $key) : mixed
+    protected final function get(string $key): mixed
     {
         return self::vcm_data()[$key] ?? null;
     }
@@ -118,7 +125,7 @@ abstract class RequestHelper
     /**
      * By default, it's responsible for digesting the request and setting the default rules
      */
-    protected function pre_validate() : void
+    protected function pre_validate(): void
     {
         self::vcm_start(self::request(as_array: true), [
             'required' => true,
@@ -154,16 +161,16 @@ abstract class RequestHelper
     {
         static::$VCM_INSTANCE = $this;
 
-        if($validate)
+        if ($validate)
             $this->validate();
     }
 
-    public final function __get(string $key) : mixed
+    public final function __get(string $key): mixed
     {
         return $this->data[$key] ?? null;
     }
 
-    public final function __isset(string $key) : bool
+    public final function __isset(string $key): bool
     {
         return isset($this->data[$key]);
     }
@@ -182,39 +189,39 @@ abstract class RequestHelper
      * @throws \Exception
      */
     public static function request(
-        bool $throw_error = true,
-        bool $as_array = false,
-        bool $invalidate_cache = false,
+        bool   $throw_error = true,
+        bool   $as_array = false,
+        bool   $invalidate_cache = false,
         ?array $mock_data = null
     ): array|object
     {
-        if(isset(self::$cached_request_fd) and !$invalidate_cache)
-            return self::$cached_request_fd;
+        if (isset(self::$cached_request_form_data) and !$invalidate_cache)
+            return self::$cached_request_form_data;
 
-        if($mock_data)
-            return self::$cached_request_fd = $as_array ? $mock_data : (object) $mock_data;
+        if ($mock_data)
+            return self::$cached_request_form_data = $as_array ? $mock_data : (object)$mock_data;
 
         $req_method = $_SERVER['REQUEST_METHOD'] ?? 'NON-SPECIFIED';
 
         $data = file_get_contents("php://input");
 
-        if($req_method != "POST") {
+        if ($req_method != "POST") {
             $content_type = explode(";", App::get_header('Content-Type'))[0];
 
-            if($req_method == "PUT" && $content_type == "multipart/form-data")
+            if ($req_method == "PUT" && $content_type == "multipart/form-data")
                 Exception::throw_exception("PUT method is not allowed for multipart/form-data requests", "LayObject::ERR");
 
 //            parse_str($data, $data);
         }
 
         $msg = "No values found in request; check if you actually sent your values as \$_POST";
-        $post = $as_array ? $_POST : (object) $_POST;
+        $post = $as_array ? $_POST : (object)$_POST;
 
         if (!empty($data) && !str_starts_with($data, "{")) {
-            if((is_object($post) && empty(get_object_vars($post))) || empty($post)) {
+            if ((is_object($post) && empty(get_object_vars($post))) || empty($post)) {
                 $post = [];
                 @parse_str($data, $post);
-                $post = $as_array ? $post : (object) $post;
+                $post = $as_array ? $post : (object)$post;
             }
 
             $msg = "JSON formatted \$_POST needed; but invalid JSON format was found";
@@ -223,8 +230,26 @@ abstract class RequestHelper
         if ($throw_error && empty($data) && empty($post))
             Exception::throw_exception($msg, "LayObject::ERR");
 
-        self::$cached_request_fd =  json_decode($data, $as_array) ?? $post;
+        self::$cached_request_form_data = json_decode($data, $as_array) ?? $post;
 
-        return self::$cached_request_fd;
+        return self::$cached_request_form_data;
+    }
+
+    /**
+     * Get $_FILES from the files object, organize it according to request $_FILES name
+     * @param bool $as_array
+     * @param bool $invalidate_cache
+     * @return array|object
+     */
+    public static function files(bool $as_array = false, bool $invalidate_cache = false): array|object
+    {
+        if (isset(self::$cached_request_files) and !$invalidate_cache)
+            return self::$cached_request_files;
+
+        if (empty($_FILES)) return $as_array ? [] : (object)[];
+
+        self::$cached_request_files = $as_array ? LayArray::to_array($_FILES) : LayArray::to_object($_FILES);
+
+        return self::$cached_request_files;
     }
 }
