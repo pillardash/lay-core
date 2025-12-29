1f203a8bf607b352409e11c4f900c4eb4bda023d
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-12-29 21:56:15 +0100

    Updated srcfilter to auto swap http for server links

--- src/Core/View/SrcFilter.php
+++ src/Core/View/SrcFilter.php
@@ -1,28 +1,50 @@
-<?php
 
-namespace BrickLayer\Lay\Core\View;
+<?php namespace BrickLayer\Lay\Core\View;
 
 final class SrcFilter
 {
-    public static function go(string $src) : string {
+    public static function go(string $src, bool $use_client = true): string
+    {
         $client = DomainResource::get();
 
-        return str_replace(
+        $x = str_replace(
             [
-                "@shared/",             "@#/",              "@static/",
-                "@shared_js/",          "@js/",             "@static_env/",
-                "@shared_img/",         "@img/",            "@ui/",
-                "@shared_css/",         "@css/",
-                "@shared_plugins/",     "@plugins/",
+                "@shared/",
+                "@#/",
+                "@static/",
+                "@shared_js/",
+                "@js/",
+                "@static_env/",
+                "@shared_img/",
+                "@img/",
+                "@ui/",
+                "@shared_css/",
+                "@css/",
+                "@shared_plugins/",
+                "@plugins/",
             ],
             [
-                $client->shared->root,      $client->root,      $client->static,
-                $client->shared->js,        $client->js,        $client->static_env,
-                $client->shared->img,       $client->img,       $client->ui,
-                $client->shared->css,       $client->css,
-                $client->shared->plugins,   $client->plugins,
+                $client->shared->root,
+                $client->root,
+                $client->static,
+                $client->shared->js,
+                $client->js,
+                $client->static_env,
+                $client->shared->img,
+                $client->img,
+                $client->ui,
+                $client->shared->css,
+                $client->css,
+                $client->shared->plugins,
+                $client->plugins,
             ],
-            $src
+            $src,
         );
+
+        if ($use_client) {
+            return $x;
+        }
+
+        return str_replace($client->root, $client->domain->public, $x);
     }
 }
